75c7dcc8830fd12e218f945854803181577f7112
Jasper Grant <jasperbengrant@gmail.com>
2024-08-12 12:45:23 -0300

    Add plotting to wav_check.py

--- scripts/wav_check.py
+++ scripts/wav_check.py
@@ -3,9 +3,16 @@
 
 import wave
 import array
+import matplotlib.pyplot as plt
 
 wav_file = wave.open("wav/qam.wav", "rb")
 
 wav_file_data = array.array("d", wav_file.readframes(wav_file.getnframes()))
 
-print(wav_file_data)
+# Plot the data
+plt.figure()
+plt.plot(wav_file_data)
+plt.title("Wav File Data")
+plt.xlabel("Sample")
+plt.ylabel("Amplitude")
+plt.show()
